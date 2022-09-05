a3fcb45f504f492f990a373bf1387197e5b90d1d
Agailh <agailham86@gmail.com>
2022-09-05 15:47:31 +0700

    add update post function

--- pages/index.js
+++ pages/index.js
@@ -13,10 +13,29 @@ const Home = () => {
     getPosts();
   }, []);
 
+  const addPost = async () => {
+    const post = { title: "New Post", body: "new" };
+    axios.post(apiEndPoint, post);
+    await axios.post(apiEndPoint, post);
+    setPosts([post, ...posts]);
+  };
+
+  const handleUpdate = async (post) => {
+    post.title = "update title";
+    await axios.put(apiEndPoint + "/" + post.id);
+    const postsClone = [...posts];
+    const index = postsClone.indexOf(post);
+    postsClone[index] = { ...post };
+    setPosts(postsClone);
+  };
+
   return (
     <>
       <div className="container">
         <h2>there are {posts.length} post in the database </h2>
+        <button onClick={addPost} className=" btn btn-primary">
+          Add Post
+        </button>
         <table className="table">
           <thead>
             <tr>
@@ -30,7 +49,10 @@ const Home = () => {
               <tr>
                 <td key={posts.id}>{posts.title}</td>
                 <td>
-                  <button className="btn btn-info btn-sm "> Update </button>
+                  <button onClick={() => handleUpdate(posts)} className="btn btn-info btn-sm ">
+                    {" "}
+                    Update{" "}
+                  </button>
                 </td>
                 <td>
                   <button className="btn btn-danger btn-sm "> Delete </button>
